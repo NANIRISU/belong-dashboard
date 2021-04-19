2403bc88cbb2182a06fe0d5d48f1d89de66f11a6
NANIRISU <muppasaikrishna@gmail.com>
2021-04-19 13:23:12 +0530

    added text for nav

--- src/component/nav/NewNav.js
+++ src/component/nav/NewNav.js
@@ -5,10 +5,7 @@ class NewNav extends Component {
 		super(props);
 		this.state = {
 			showPara:false,
-			showInformation:false,
-			showTask:false,
-			showResource:false,
-			showSolution:false
+			
 		}
 		this.btnClick = this.btnClick.bind(this)
 		this.Information=this.Information.bind(this);
@@ -20,27 +17,51 @@ class NewNav extends Component {
 
     btnClick(){
 		this.setState({
-			showPara:true
+			showPara:true,
+			showInformation:false,
+			showTask:false,
+			showResource:false,
+			showSolution:false
 		})
 	}
 	Information(){
 		this.setState({
-			showInformation:true
+
+			showInformation:true,
+			showPara:false,
+		
+			showTask:false,
+			showResource:false,
+			showSolution:false
 		})
 	}
 	Task(){
 		this.setState({
-			showTask:true
+			showTask:true,
+			showInformation:false,
+		showPara:false,
+			showResource:false,
+			showSolution:false
 		})
 	}
 	Resources(){
 		this.setState({
-			showResource:true
+			showPara:false,
+			showResource:true,
+			showInformation:false,
+			showTask:false,
+			showSolution:false
+
 		})
 	}
 	Solution(){
 		this.setState({
-			showSolution:true
+			showSolution:true,
+			showPara:false,
+			showInformation:false,
+			showTask:false,
+			showResource:false,
+			
 		})
 	}
 
@@ -53,11 +74,11 @@ class NewNav extends Component {
 				<button id="click" onClick = {this.Task}>task</button>
 		        <button id="click" onClick = {this.Resources}>Resources</button>
 				<button id="click" onClick = {this.Solution}>Solution</button>
-				{this.state.showPara && <p id="para">hello Mentor </p>}
+				{this.state.showPara && <p id="para">hello Mentor <br/> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>}
 				{this.state.showInformation && <p id="para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>}
-				{this.state.showTask && <p id="para">hello Task </p>}
-				{this.state.showResource && <p id="para">hello  Resources</p>}
-				{this.state.showSolution && <p id="para">hello  Solution</p>}
+				{this.state.showTask && <p id="para">hello Task <br/> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>}
+				{this.state.showResource && <p id="para">hello  Resources <br/> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>}
+				{this.state.showSolution && <p id="para">hello  Solution <br/> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>}
     		</div>
     	);
     }
